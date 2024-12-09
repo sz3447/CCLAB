7b2043617328362cf6fc8e516f8fbca441637b15
SZOU <sz3447@nyu.edu>
2024-12-09 23:01:32 +0800

    replace button css with js

--- ProjectB/js/intermediate.js
+++ ProjectB/js/intermediate.js
@@ -1,3 +1,17 @@
-function redirectToNext() {
-    window.location.href = "main.html";
+let bttn;
+function setup() {
+  createCanvas(400, 400);
+  bttn = createButton ("GO TO ANOTHER PAGE");
+  bttn.mousePressed(redirect);
+  bttn.size(200, 100);
+
+}
+
+function draw() {
+  background(220);
+
+}
+
+function redirect(){
+  window.location.href = "main-game.html";
 }
